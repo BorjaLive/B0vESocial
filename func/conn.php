5d12ed9282040f65472fec95300febfd0b605ca1
BorjaLive <borjainlive@gmail.com>
2021-12-09 23:56:59 +0100

    Eliminado cejamiento y ahora la api normal permite crear posts sencillos

--- func/conn.php
+++ func/conn.php
@@ -1,15 +1,12 @@
 <?php
     include "constants.php";
     use PayPal\Api\Amount;
-    use PayPal\Api\Details;
     use PayPal\Api\Item;
     use PayPal\Api\ItemList;
     use PayPal\Api\Payer;
     use PayPal\Api\Payment;
     use PayPal\Api\RedirectUrls;
     use PayPal\Api\Transaction;
-    use \PayPal\Rest\ApiContext;
-    use \PayPal\Auth\OAuthTokenCredential;
 
     $link = conn();
 
